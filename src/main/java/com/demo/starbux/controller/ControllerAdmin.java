7d29a50ec310efe7210f0960300d7cd384242e29
Gökalp Rençber <grencber@gmail.com>
2021-08-31 12:38:30 +0300

    admin api 1st phase implemented

--- src/main/java/com/demo/starbux/controller/ControllerAdmin.java
+++ src/main/java/com/demo/starbux/controller/ControllerAdmin.java
@@ -1,30 +1,53 @@
 package com.demo.starbux.controller;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demo.starbux.domain.CombinationToppingItem;
-import com.demo.starbux.domain.DrinkCombination;
-import com.demo.starbux.domain.Order;
-import com.demo.starbux.domain.cart.CartDrink;
-import com.demo.starbux.repositories.CombinationToppingItemRepo;
-import com.demo.starbux.repositories.DrinkCombinationRepo;
+import com.demo.starbux.domain.Item;
 import com.demo.starbux.repositories.ItemRepo;
-import com.demo.starbux.repositories.OrderRepo;
-import com.demo.starbux.repositories.implementations.ItemRepoInterfaceImpl;
-import com.demo.starbux.service.MenuService;
 
 @RestController
 @RequestMapping("/admin")
 public class ControllerAdmin {
 	
-
-
+	@Autowired
+	private ItemRepo itemRepo;
 	
+	@PostMapping("/create")
+	public ResponseEntity<String> create(@RequestBody Item requestedItem) {
+		Item itemToSave = new Item(requestedItem.getItemName(), requestedItem.getItemType(), requestedItem.getItemPrice());
+		itemRepo.save(itemToSave);
+		return ResponseEntity.status(HttpStatus.OK).body("New item is added to Item List");
+	}
+
+	@PutMapping("/update")
+//	@PatchMapping("/patch")
+	public ResponseEntity<Item> update(@RequestBody Item updateItem) {
+		Item itemToReplace = new Item(updateItem.getItemName(), updateItem.getItemType(), updateItem.getItemPrice());
+		Item updatedItem = itemRepo.save(itemToReplace);
+		ResponseEntity.status(HttpStatus.OK).build();
+		return ResponseEntity.ok(updatedItem);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> delete(@PathVariable Integer id) {
+		Optional<Item> itemToBeDeleted = itemRepo.findById(id);
+		if (!itemToBeDeleted.isEmpty() & !itemToBeDeleted.equals(null)) {
+			itemRepo.deleteById(id);
+			return ResponseEntity.ok("Item is deleted");
+		}
+		return ResponseEntity.notFound().build();
+		
+	}
 }
